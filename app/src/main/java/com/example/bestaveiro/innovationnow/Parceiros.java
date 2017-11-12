3f4ebd86014f14a1e858fafa74ed1c83b6ed955a
Jean Brito <jean18.jh@gmail.com>
2017-11-12 15:46:22 +0000

    update

--- app/src/main/java/com/example/bestaveiro/innovationnow/Parceiros.java
+++ app/src/main/java/com/example/bestaveiro/innovationnow/Parceiros.java
@@ -11,69 +11,33 @@ public class Parceiros extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_parceiros);
+        setContentView(R.layout.activity_parceiros);    }
+
+
+    public void gototugaBox(View view) { goToUrl("https://www.tugabox.pt/?lang=en-en");    }
+
+    public void gotoitGrow(View view) { goToUrl("www.itgrow.pt/");    }
+
+    public void gotoshoyce(View view) { goToUrl("shoyce.pt/pt/");    }
+
+    public void gotomirtiflor(View view) { goToUrl("https://pt-pt.facebook.com/Mirtiflor-745004355521875/");    }
+
+
+
+    public void gotoua(View view) {
+        goToUrl("https://www.ua.pt/");
     }
 
-    public void gotoefacec (View view) {
-        goToUrl ( "http://www.efacec.pt/");
+    public void gotoipdj(View view) {
+        goToUrl("http://www.ipdj.pt/");
     }
-    public void gotomicro (View view) {
-        goToUrl ( "https://www.microsoft.com/pt-pt/");
-    }
-    public void gotokic1 (View view) {
-        goToUrl ( "https://eit.europa.eu/eit-community/kic-innoenergy");
-    }
-    public void gotohovione (View view) {
-        goToUrl ( "http://www.hovione.pt/");
-    }
-    public void gotoogmedical (View view) {
-        goToUrl ( "http://ogmedical.pt/index.php?lang=pt&mod=home");
-    }
-    public void gotologoporto (View view) {
-        goToUrl ( "http://www.myportocruz.com/");
-    }
-    public void gotovadia (View view) {
-        goToUrl ( "http://www.cervejavadia.pt/site/");
-    }
-    public void gotofrei (View view) {
-        goToUrl ( "http://freituck.com/");
-    }
-    public void gotocanal (View view) {
-        goToUrl ( "http://www.canalsuperior.pt/");
-    }
-    public void gotodoit (View view) {
-        goToUrl ( "http://www.doit.pt/");
-    }
-    public void gotoasf (View view) {
-        goToUrl ( "http://www.asformacao.com/");
-    }
-    public void gotokopy (View view) {
-        goToUrl ( "http://www.kopystation.pt/");
-    }
-    public void gotoholmes (View view) {
-        goToUrl ( "http://holmesplace.pt/pt/");
-    }
-    public void gotoprint2 (View view) {
-        goToUrl ( "http://www.print4fun3d.com/");
-    }
-    public void gotoindasa (View view) {
-        goToUrl ( "https://www.indasa-abrasives.com/");
-    }
-    public void gotolatina (View view) {
-        goToUrl ( "https://www.facebook.com/pastelarialatina.aveiro/");
-    }
-    public void gotoordem (View view) {
-        goToUrl ( "http://www.ordemengenheiros.pt/pt/");
-    }
-    public void gotoua (View view) {
-        goToUrl ( "https://www.ua.pt/");
-    }
-    public void gotoipd (View view) {
-        goToUrl ( "http://www.ipdj.pt/");
+
+    public void gotoaauav(View view) {
+        goToUrl("http://www.aauav.pt/");
     }
 
 
-    private void goToUrl (String url) {
+    private void goToUrl(String url) {
         Uri uriUrl = Uri.parse(url);
         Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
         startActivity(launchBrowser);
